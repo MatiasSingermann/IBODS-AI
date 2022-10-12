1ae760e6c40d53732f211c5f35a69f3721d0342c
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-10-11 22:10:57 -0300

    Update: Added sidebar, with slider and selectbox

--- app_deploy.py
+++ app_deploy.py
@@ -5,3 +5,7 @@ import streamlit as st
 with st.container():
     st.title("IBODS Project Deployement")
     st.write("Esta es la pagina de pruebas para nuestro proyecto")
+
+thr = st.sidebar.slider("Detection Threshold", min_value = 0.0, max_value = 1.0, value = 0.3, step = 0.01)
+
+model = st.sidebar.selectbox("Select Model",  ("EfficientDet0", "EfficientDet1"))
